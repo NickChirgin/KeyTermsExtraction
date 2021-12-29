0f5eea2c6c47289675b5b23bcc44f8279e30b199
Nikolay Chirgin <nickchirgin@gmail.com>
2021-12-29 14:59:12 +0300

    Second Commit

--- keyterms.py
+++ keyterms.py
@@ -1,21 +1,21 @@
-from nltk.tokenize import word_tokenize
-from lxml import etree
+import string
 from collections import Counter
 
-xml = "news.xml"
-root = etree.parse(xml).getroot()
-result = ""
-for j in range(0, len(root[0])):
-    for i in root[0][j]:
-        result = ""
-        if i.get('name') == 'head':
-            print(f"{i.text}:")
-        if i.get('name') == 'text':
-            sentence = i.text
-            sentence = word_tokenize(sentence.lower())
-            sentence = sorted(sentence, reverse=True)
-            frequent = Counter(sentence)
-            frequent = frequent.most_common(5)
-            for word in frequent:
-                result += word[0] + " "
-            print(result[:-1])
+from lxml import etree
+from nltk import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+root = etree.parse("news.xml").getroot()
+lemmatizer = WordNetLemmatizer()
+
+for news in root[0]:
+    print(f"{news[0].text}:")
+    counts = Counter()
+    for word in word_tokenize(news[1].text.lower()):
+        word = lemmatizer.lemmatize(word)
+        if word not in string.punctuation and word not in stopwords.words("english"):
+            counts.update([word])
+    counts_sorted = sorted(counts.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
+    print(" ".join(x[0] for x in counts_sorted[:5]))
+    print()
