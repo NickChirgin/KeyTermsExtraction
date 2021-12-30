ed1aec0c5d6dc2c2bd6f6f717e2dceb43666d13b
Nikolay Chirgin <nickchirgin@gmail.com>
2021-12-30 19:57:07 +0300

    Fourth Commit

--- keyterms.py
+++ keyterms.py
@@ -1,21 +1,40 @@
 import string
 from collections import Counter
-
+import pandas as pd
+import nltk
+from nltk.tag.perceptron import PerceptronTagger
 from lxml import etree
 from nltk import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 
 root = etree.parse("news.xml").getroot()
+headlines = []
 lemmatizer = WordNetLemmatizer()
-
+stop_words = stopwords.words('english') + list(string.punctuation)
+vectorizer = TfidfVectorizer()
+vocabulary = ["", "", "", "", "", "", "", "", "", ""]
+i = 0
+list_to_print = []
 for news in root[0]:
-    print(f"{news[0].text}:")
+    headlines.append(f"{news[0].text}:")
     counts = Counter()
+    text = ""
     for word in word_tokenize(news[1].text.lower()):
         word = lemmatizer.lemmatize(word)
         if word not in string.punctuation and word not in stopwords.words("english"):
-            counts.update([word])
+            if nltk.pos_tag([word])[0][1] == 'NN':
+                counts.update([word])
+                text += word + " "
+    vocabulary[i] = text
+    i += 1
     counts_sorted = sorted(counts.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
-    print(" ".join(x[0] for x in counts_sorted[:5]))
-    print()
+tfidf_matrix = vectorizer.fit_transform(vocabulary)
+terms = vectorizer.get_feature_names_out()
+for i in range(len(headlines)):
+    print(headlines[i])
+    df = pd.DataFrame(tfidf_matrix[i].T.todense(), index=terms, columns=["tfidf"])
+    df.index.name = 'word'
+    df = df.sort_values(['tfidf', 'word'], ascending=[False, False]).head(5)
+    print(' '.join(list(df.index)), '\n')
